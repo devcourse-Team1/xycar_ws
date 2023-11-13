2d1950c5f4db37edbfb5c6c526860b2912ec3998
HeyLip <dlgkfla007@gmail.com>
2023-11-13 14:14:17 +0900

    modified GAIN value in yaml

--- src/LaneKeepingSystem/src/LaneKeepingSystem/LaneKeepingSystem.cpp
+++ src/LaneKeepingSystem/src/LaneKeepingSystem/LaneKeepingSystem.cpp
@@ -47,8 +47,9 @@ template <typename PREC>
 void LaneKeepingSystem<PREC>::run()
 {
     ros::Rate rate(kFrameRate);
-    double pos_diff;
-    
+    PREC pos_diff;
+    PREC angle;
+
     while (ros::ok())
     {
         ros::spinOnce();
@@ -61,9 +62,9 @@ void LaneKeepingSystem<PREC>::run()
         pos_diff = mLaneDetector->Hough(mFrame);
         std::cout << "pos_diff : " << pos_diff << "\n";
 
-        mPid->getControlOutput(pos_diff);
-        speedControl(pos_diff);
-        drive(pos_diff);
+        angle = mPid->getControlOutput(pos_diff);
+        speedControl(angle);
+        drive(angle);
     }
 }
 
